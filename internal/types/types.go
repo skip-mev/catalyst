f86e308eba4492c174fc785a37e22359597ccf5b
nadimabdelaziz <nadeem.abdelaziz99@gmail.com>
2025-04-15 16:45:49 -0400

    add new fields to unmarshal yaml

--- internal/types/types.go
+++ internal/types/types.go
@@ -165,6 +165,8 @@ func (s *LoadTestSpec) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		GasDenom            string        `yaml:"gas_denom"`
 		Bech32Prefix        string        `yaml:"bech32_prefix"`
 		Msgs                []LoadTestMsg `yaml:"msgs"`
+		UnorderedTxs        bool          `yaml:"unordered_txs"`
+		TxTimeout           time.Duration `yaml:"tx_timeout"`
 	}
 
 	var aux LoadTestSpecAux
@@ -182,6 +184,8 @@ func (s *LoadTestSpec) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		GasDenom:            aux.GasDenom,
 		Bech32Prefix:        aux.Bech32Prefix,
 		Msgs:                aux.Msgs,
+		UnorderedTxs:        aux.UnorderedTxs,
+		TxTimeout:           aux.TxTimeout,
 	}
 
 	return nil
