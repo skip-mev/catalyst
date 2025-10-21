66b0692b6f4e7fc1c6aef2ae5fdf4111062f3c40
Matt Acciai <matt@cosmoslabs.io>
2025-10-21 13:38:35 -0400

    adjust cache config to its own struct

--- chains/types/spec.go
+++ chains/types/spec.go
@@ -21,8 +21,24 @@ type LoadTestSpec struct {
 	Msgs         []LoadTestMsg `yaml:"msgs" json:"msgs"`
 	TxTimeout    time.Duration `yaml:"tx_timeout,omitempty" json:"tx_timeout,omitempty"`
 	ChainCfg     ChainConfig   `yaml:"-" json:"-"` // decoded via custom UnmarshalYAML
-	WalletCache  string        `yaml:"wallet_cache" json:"wallet_cache"`
-	TxCache      string        `yaml:"tx_cache" json:"tx_cache"`
+	Cache        CacheConfig   `yaml:"cache_config" json:"cache_config"`
+}
+
+type CacheConfig struct {
+	WalletsFile        string `yaml:"wallets_file" json:"wallets_file"`
+	TxsFile            string `yaml:"txs_file" json:"txs_file"`
+	ShouldCacheTxs     bool   `yaml:"should_cache_txs" json:"should_cache_txs"`
+	ShouldCacheWallets bool   `yaml:"should_cache_wallets" json:"should_cache_wallets"`
+}
+
+func (cc CacheConfig) Validate() error {
+	if cc.ShouldCacheWallets && cc.WalletsFile == "" {
+		return fmt.Errorf("wallets_file must be specified if should_cache_wallets is true")
+	}
+	if cc.ShouldCacheTxs && cc.TxsFile == "" {
+		return fmt.Errorf("txs_file must be specified if should_cache_txs is true")
+	}
+	return nil
 }
 
 type loadTestSpecAlias LoadTestSpec
@@ -78,5 +94,13 @@ func (s *LoadTestSpec) Validate() error {
 		return fmt.Errorf("NumWallets must be greater than zero")
 	}
 
-	return s.ChainCfg.Validate(*s)
+	if err := s.ChainCfg.Validate(*s); err != nil {
+		return fmt.Errorf("validating chain config: %w", err)
+	}
+
+	if err := s.Cache.Validate(); err != nil {
+		return fmt.Errorf("validating cache config: %w", err)
+	}
+
+	return nil
 }
